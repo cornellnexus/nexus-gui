99ae747c8f6dee316559f46693546393c95ccfbd
span1012 <72399906+span1012@users.noreply.github.com>
2023-09-23 14:31:51 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,10 +16,13 @@ function App() {
   const [travType, setTravType] = useState("lawnmower");
   const [baseLat, setBaseLat] = useState();
   const [baseLong, setBaseLong] = useState();
+  const [gridLong, setGridLong] = useState();
+  const [gridLat, setGridLat] = useState();
+
 
   const [errorMessage, setErrorMessage] = useState("");
   const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
 
   // Posts setup data to backend, which will try to initialize handshake
   // If successful, changes form page to mission
@@ -40,7 +43,7 @@ function App() {
         username: username,
         password: password,
         ip: ip
-    }
+      }
     }).then(response => {
       setLoading(false);
       setPage(1)
@@ -76,115 +79,134 @@ function App() {
 
   return (
     <>
-    <div className='App'>
-      <div className='header'>
-        <div className='logo'>
-          <img style={{"width": '150px', 'marginLeft': '50px', 'marginTop': '10px'}} src={require('./Images/logo.png')} />
+      <div className='App'>
+        <div className='header'>
+          <div className='logo'>
+            <img style={{ "width": '150px', 'marginLeft': '50px', 'marginTop': '10px' }} src={require('./Images/logo.png')} />
+          </div>
         </div>
-      </div>
 
-      {page === 0 ?
-      <div className='form-container'>
-        <div className='form'>
-          <h2>Setup</h2>
-          <form onSubmit={handleSetup}>
-            <div className='input-box'>
-              <input type={"text"} value={"nexus"} placeholder={"Username"} onChange={(e) => setUsername(e.target.value)} required />
+        {page === 0 ?
+          <div className='form-container'>
+            <div className='form idk'>
+              <h2>Setup</h2>
+              <form onSubmit={handleSetup}>
+                <div className='input-box'>
+                  <input type={"text"} value={"nexus"} placeholder={"Username"} onChange={(e) => setUsername(e.target.value)} required />
+                </div>
+
+                <div className='input-box'>
+                  <input type={"password"} value={"raspberry"} placeholder={"Password"} onChange={(e) => setPassword(e.target.value)} required />
+                </div>
+
+                <div className='input-box'>
+                  <input type={"text"} placeholder={"IP Address"} onChange={(e) => setIP(e.target.value)} required />
+                </div>
+
+                <div className='errorMessage'>
+                  {errorMessage}
+                </div>
+
+                {loading ?
+                  <div className='input-box button loading'>
+                    <input style={{ "color": "white" }} type={"submit"} value="Connecting to Robot..." />
+                  </div>
+                  :
+                  <div className='input-box button'>
+                    <input style={{ "color": "white" }} type={"submit"} value="Connect to Robot" />
+                  </div>
+                }
+
+              </form>
             </div>
+          </div>
+          :
+          <div className='form-container'>
+            <div className='form '>
+              <form style={{ 'marginTop': '0px' }} onSubmit={handleMission}>
+                <div className='mission-row'>
+                  <div className='col'>
+                    <h2>Robot</h2>
+                    <div className='input-box'>
+                      <input type={"text"} placeholder={"Robot Latitude"} onChange={(e) => setLat(e.target.value)} required />
+                    </div>
 
-            <div className='input-box'>
-              <input type={"password"} value={"raspberry"} placeholder={"Password"} onChange={(e) => setPassword(e.target.value)} required />
+                    <div className='input-box'>
+                      <input type={"text"} placeholder={"Robot Longitude"} onChange={(e) => setLong(e.target.value)} required />
+                    </div>
+
+                    <h2>Traversal Type</h2>
+
+                    <div>
+                      <input className='radio' type={"radio"} value={"lawnmower"} checked={travType === "lawnmower"} onChange={(e) => {
+                        setTravType(e.target.value)
+                      }} />
+                      <label htmlFor='Lawnmower'>Lawnmower</label>
+                    </div>
+
+                    <div>
+                      <input className='radio' type={"radio"} value={"spiral"} checked={travType === "spiral"} onChange={(e) => {
+                        setTravType(e.target.value)
+                      }} />
+                      <label htmlFor='Spiral'>Spiral</label>
+                    </div>
+
+                    <div>
+                      <input className='radio' type={"radio"} value={"lawnmower b"} checked={travType === "lawnmower b"} onChange={(e) => {
+                        setTravType(e.target.value)
+                      }} />
+                      <label htmlFor='Lawnmower B'>Lawnmower B</label>
+                    </div>
+
+
+
+                  </div>
+                  <div className='right col'>
+                    <h2>Grid</h2>
+                    {/* still need to change the actual methods!*/}
+                    <div className='input-box'>
+                      <input type={"text"} placeholder={"Latitude"} onChange={(e) => setGridLat(e.target.value)} required />
+                    </div>
+
+                    <div className='input-box'>
+                      <input type={"text"} placeholder={"Longitude"} onChange={(e) => setGridLong(e.target.value)} required />
+                    </div>
+
+
+
+                    <h2>Base Station</h2>
+
+                    <div className='input-box'>
+                      <input type={"text"} placeholder={"Base Station Latitude"} onChange={(e) => setBaseLat(e.target.value)} required />
+                    </div>
+
+                    <div className='input-box'>
+                      <input type={"text"} placeholder={"Base Station Longitude"} onChange={(e) => setBaseLong(e.target.value)} required />
+                    </div>
+
+                    <div className='errorMessage'>
+                      {errorMessage}
+                    </div>
+
+                  </div>
+
+                </div>
+
+                {loading ?
+                  <div className='input-box button loading'>
+                    <input style={{ "color": "white" }} type={"submit"} value="Initializing Robot..." />
+                  </div>
+                  :
+                  <div className='input-box button'>
+                    <input style={{ "color": "white" }} type={"submit"} value="Initialize Robot" />
+                  </div>
+                }
+              </form>
             </div>
+          </div>
+        }
 
-            <div className='input-box'>
-              <input type={"text"} placeholder={"IP Address"} onChange={(e) => setIP(e.target.value)} required />
-            </div>
-
-            <div className='errorMessage'>
-              {errorMessage}
-            </div>
-
-            {loading ? 
-            <div className='input-box button loading'>
-              <input style={{"color": "white"}} type={"submit"} value="Connecting to Robot..." />
-            </div>
-            : 
-            <div className='input-box button'>
-              <input style={{"color": "white"}} type={"submit"} value="Connect to Robot" />
-            </div>
-            }
-
-        </form>
-      </div>
-      </div>
-    :
-    <div className='form-container'>
-        <div className='form'>
-          <h2>Robot</h2> 
-          <form style={{'marginTop': '0px'}} onSubmit={handleMission}>
-            <div className='input-box'>
-              <input type={"text"} placeholder={"Robot Latitude"} onChange={(e) => setLat(e.target.value)} required />
-            </div>
-
-            <div className='input-box'>
-              <input type={"text"} placeholder={"Robot Longitude"} onChange={(e) => setLong(e.target.value)} required />
-            </div>
-
-            <h2>Traversal Type</h2>
-
-            <div>
-              <input className='radio' type={"radio"} value={"lawnmower"} checked={travType === "lawnmower"} onChange={(e) => {
-                setTravType(e.target.value)
-              }} />
-              <label htmlFor='Lawnmower'>Lawnmower</label>
-            </div>
-
-            <div>
-              <input className='radio' type={"radio"} value={"spiral"} checked={travType === "spiral"} onChange={(e) => {
-                setTravType(e.target.value)
-              }} />
-              <label htmlFor='Spiral'>Spiral</label>
-            </div>
-
-            <div>
-              <input className='radio' type={"radio"} value={"lawnmower b"} checked={travType === "lawnmower b"} onChange={(e) => {
-                setTravType(e.target.value)
-              }} />
-              <label htmlFor='Lawnmower B'>Lawnmower B</label>
-            </div>
-
-            <h2>Grid</h2>
-
-            <h2>Base Station</h2>
-
-            <div className='input-box'>
-              <input type={"text"} placeholder={"Base Station Latitude"} onChange={(e) => setBaseLat(e.target.value)} required />
-            </div>
-
-            <div className='input-box'>
-              <input type={"text"} placeholder={"Base Station Longitude"} onChange={(e) => setBaseLong(e.target.value)} required />
-            </div>
-
-            <div className='errorMessage'>
-              {errorMessage}
-            </div>
-
-            {loading ? 
-            <div className='input-box button loading'>
-              <input style={{"color": "white"}} type={"submit"} value="Initializing Robot..." />
-            </div>
-            : 
-            <div className='input-box button'>
-              <input style={{"color": "white"}} type={"submit"} value="Initialize Robot" />
-            </div>
-            }
-
-        </form>
-      </div>
-      </div>
-      }
-      
-    </div>
+      </div >
     </>
   );
 }
