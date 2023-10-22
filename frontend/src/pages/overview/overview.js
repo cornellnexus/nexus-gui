6bef36a21182a574f29115b619444d3eef59409a
Vinny <vh222@cornell.edu>
2023-10-21 23:23:29 -0400

    Sample layout
    + added placeholder values for what GUI would look like when we do grab data from robot
    + created outline for expandComponent function that will expand component from generic view to detailed view on click

--- frontend/src/pages/overview/overview.js
+++ frontend/src/pages/overview/overview.js
@@ -4,6 +4,10 @@ import Clock from "../../components/clock/clock";
 
 // https://www.youtube.com/watch?v=wYpCWwD1oz0
 
+function expandComponent() {
+  console.log("Click on div detected");
+}
+
 function Overview() {
   return (
     <div className="Overview">
@@ -16,14 +20,22 @@ function Overview() {
         </div>
         <h1 id="Overview-title">Overview</h1>
         <div className="Subcomponent-wrapper">
-          <div className="Subcomponent">
+          <div className="Subcomponent" onClick={expandComponent}>
             <h3 className="Subcomponent-title">Battery</h3>
+            <p>Battery Level: 54%</p>
+            <p>(Battery-Life Remaining)</p>
           </div>
-          <div className="Subcomponent">
+          <div className="Subcomponent" onClick={expandComponent}>
             <h3 className="Subcomponent-title">Sensors</h3>
+            <p>Velocity: 12 mph</p>
+            <p>Acceleration: +3 mph</p>
+            <p>Location: (Lat, Long)</p>
+            <p>Bucket Status: Full, Half-Full, Not Full</p>
           </div>
-          <div id="Final-subcomponent">
+          <div id="Final-subcomponent" onClick={expandComponent}>
             <h3 className="Subcomponent-title">Metrics</h3>
+            <p>Goal Location: (Lat, Long)</p>
+            <p>Robot Dist. to Goal: 3.537 meters</p>
           </div>
         </div>
       </div>
