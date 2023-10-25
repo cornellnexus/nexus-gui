9a966239149347d5b9b5bd81e6732e0e74df0812
Vinny <vh222@cornell.edu>
2023-10-25 18:37:10 -0400

    Code refactoring
    + made onClick into arrow function so that the expandComponent isn't being created every time the page is rendered
    + CSS changes

--- frontend/src/pages/overview/overview.js
+++ frontend/src/pages/overview/overview.js
@@ -4,10 +4,6 @@ import Clock from "../../components/clock/clock";
 
 // https://www.youtube.com/watch?v=wYpCWwD1oz0
 
-function expandComponent() {
-  console.log("Click on div detected");
-}
-
 function Overview() {
   return (
     <div className="Overview">
@@ -20,22 +16,28 @@ function Overview() {
         </div>
         <h1 id="Overview-title">Overview</h1>
         <div className="Subcomponent-wrapper">
-          <div className="Subcomponent" onClick={expandComponent}>
-            <h3 className="Subcomponent-title">Battery</h3>
-            <p>Battery Level: 54%</p>
-            <p>(Battery-Life Remaining)</p>
+          <div className="Subcomponent">
+            <div id="Battery" onClick={() => console.log("Clicked")}>
+              <h3 className="Subcomponent-title">Battery</h3>
+              <p>Battery Level: 54%</p>
+              <p>(Battery-Life Remaining)</p>
+            </div>
           </div>
-          <div className="Subcomponent" onClick={expandComponent}>
-            <h3 className="Subcomponent-title">Sensors</h3>
-            <p>Velocity: 12 mph</p>
-            <p>Acceleration: +3 mph</p>
-            <p>Location: (Lat, Long)</p>
-            <p>Bucket Status: Full, Half-Full, Not Full</p>
+          <div className="Subcomponent">
+            <div id="Sensors" onClick={() => console.log("Clicked")}>
+              <h3 className="Subcomponent-title">Sensors</h3>
+              <p>Velocity: 12 mph</p>
+              <p>Acceleration: +3 mph</p>
+              <p>Location: (Lat, Long)</p>
+              <p>Bucket Status: Full, Half-Full, Not Full</p>
+            </div>
           </div>
-          <div id="Final-subcomponent" onClick={expandComponent}>
-            <h3 className="Subcomponent-title">Metrics</h3>
-            <p>Goal Location: (Lat, Long)</p>
-            <p>Robot Dist. to Goal: 3.537 meters</p>
+          <div className="Final-subcomponent">
+            <div id="Metrics" onClick={() => console.log("Clicked")}>
+              <h3 className="Subcomponent-title">Metrics</h3>
+              <p>Goal Location: (Lat, Long)</p>
+              <p>Robot Dist. to Goal: 3.537 meters</p>
+            </div>
           </div>
         </div>
       </div>
